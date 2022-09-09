a59c67ffafbef97b7477d2df904a9219b1e6fdb6
amicuswat <amicuswat@gmail.com>
2022-09-09 11:40:31 +0300

    Refactoring main(), count_clicks()
    Убрал лишние переменные в main
    Убрал передачу параметров, которые заданы по умолчанию в count_clicks

--- main.py
+++ main.py
@@ -18,17 +18,13 @@ def main():
     parser.add_argument('url')
     args = parser.parse_args()
 
-    url_ = args.url
-
-    url_parsed = urlparse(url_)
-    bitlink_url = url_parsed.netloc + url_parsed.path
+    bitlink_url = urlparse(args.url).netloc + urlparse(args.url).path
 
     if is_bitlink(token, bitlink_url):
         response = count_clicks(token, bitlink_url)
-        clicks = response.json()['total_clicks']
-        print('Clicks:', clicks)
+        print('Clicks:', response.json()['total_clicks'])
     else:
-        response = shorten_link(token, url_)
+        response = shorten_link(token, args.url)
         if response.ok:
             print(response.json()['link'])
         else:
@@ -62,14 +58,8 @@ def count_clicks(token, url):
     headers = {
         'Authorization': f'Bearer {token}',
     }
-    
-    params = (
-        ('unit', 'day'),
-        ('units', '-1'),
-    )
 
-    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary', headers=headers, params=params)
-    
+    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary', headers=headers)
     return response
 
 

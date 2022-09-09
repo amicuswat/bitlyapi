e743ec9355dd889b28cc65907c395d947e8e2722
amicuswat <amicuswat@gmail.com>
2022-09-09 18:35:32 +0300

    Cleaned file according pep8(flake8)

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
         print('Clicks:', count_clicks(token, args.url))
     else:
         print('Short link:', shorten_link(token, args.url))
-   
+
 
 def is_bitlink(token, url):
     parsed_url = urlparse(url)
@@ -28,7 +28,8 @@ def is_bitlink(token, url):
         'Authorization': f'Bearer {token}',
     }
 
-    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}', headers=headers)
+    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}',
+                            headers=headers)
 
     return response.ok
 
@@ -41,11 +42,13 @@ def count_clicks(token, url):
         'Authorization': f'Bearer {token}',
     }
 
-    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/{url}/clicks/summary', headers=headers)
+    response = requests.get(f'https://api-ssl.bitly.com/v4/bitlinks/'
+                            f'{url}/clicks/summary',
+                            headers=headers)
     response.raise_for_status()
 
     return response.json()['total_clicks']
-       
+
 
 def shorten_link(token, url):
     headers = {
@@ -56,7 +59,9 @@ def shorten_link(token, url):
         "long_url": url,
     }
 
-    response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, json=long_url)
+    response = requests.post('https://api-ssl.bitly.com/v4/shorten',
+                             headers=headers,
+                             json=long_url)
     response.raise_for_status()
 
     return response.json()['link']
@@ -64,4 +69,3 @@ def shorten_link(token, url):
 
 if __name__ == '__main__':
     main()
-
